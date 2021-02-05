2b799997c82e92daa138c08bf1e7c040e7503140
steamedbao <wanruiray@hotmail.com>
2021-02-05 09:05:07 +0800

    change file path

--- entities/GridMap.java
+++ entities/GridMap.java
@@ -7,41 +7,34 @@ import java.io.IOException;
 
 import static constant.EntitiesConstants.*;
 
-
-
 public class GridMap {
-	private int rows = MAP_ROWS;
-	private int cols = MAP_COLS;
-	private GridBox[][] map;
-	
-	
-	
-	//constructors
-	public GridMap() {
-		map = new GridBox[MAP_COLS][MAP_ROWS];
+    private int rows = MAP_ROWS;
+    private int cols = MAP_COLS;
+    private GridBox[][] map;
+
+    // constructors
+    public GridMap() {
+        map = new GridBox[MAP_COLS][MAP_ROWS];
         for (int x = 0; x < MAP_COLS; x++) {
             for (int y = 0; y < MAP_ROWS; y++) {
-                map[x][y] = new GridBox(x,y);
+                map[x][y] = new GridBox(x, y);
             }
         }
         reset();
-	}
-	
-	//getters & setters
+    }
+
+    // getters & setters
 
     public GridBox[][] getCells() {
         return map;
     }
-    
 
     public static boolean isInStartZone(int x, int y) {
-        return x < ZONE_SIZE && x >= 0
-                && y < MAP_ROWS && y >= MAP_ROWS - ZONE_SIZE;
+        return x < ZONE_SIZE && x >= 0 && y < MAP_ROWS && y >= MAP_ROWS - ZONE_SIZE;
     }
 
     public static boolean isInEndZone(int x, int y) {
-        return x < MAP_COLS && x >= MAP_COLS - ZONE_SIZE
-                && y < ZONE_SIZE && y >= 0;
+        return x < MAP_COLS && x >= MAP_COLS - ZONE_SIZE && y < ZONE_SIZE && y >= 0;
     }
 
     public boolean getIsObstacle(int x, int y) {
@@ -49,7 +42,7 @@ public class GridMap {
     }
 
     public boolean getIsOnlyObstacle(int x, int y) {
-        return isOutOfArena(x,y) ? false: map[x][y].getIsObstacle();
+        return isOutOfArena(x, y) ? false : map[x][y].getIsObstacle();
     }
 
     public boolean isOutOfArena(int x, int y) {
@@ -60,7 +53,7 @@ public class GridMap {
         if (isOutOfArena(x, y))
             return;
         map[x][y].setObstacle(isObstacle);
-        
+
     }
 
     public void setExplored(int x, int y, boolean explored) {
@@ -72,87 +65,65 @@ public class GridMap {
     public boolean getIsExplored(int x, int y) {
         return !isOutOfArena(x, y) && map[x][y].getIsVisited();
     }
-	
-	
-	//preset of obstacle
-	public void presetObstacles() {
-		
-		int[][] preset = {
-				{ 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},	
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-				{ 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},	
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
-				{ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-		};
-		
-		for (int prow = 0; prow <rows; prow++ ) {
-			for(int pcol = 0; pcol < cols; pcol++) {
-				if (preset[prow][pcol] == 1)
-					map[pcol][prow].setObstacle(true);			
-			}
-		}
-		
-		
-	}
-	
-	public void printMapObstacle() {
-		//border top settings
-		for(int bt = 0; bt<cols+2 ; bt++) {
-			System.out.print("_");
-		}
-		
-		
-		for(int r = 0; r<rows; r++) {
-			System.out.println();
-			System.out.print("|");
-			for(int c=0; c<cols ; c++)
-				map[c][r].printGrid();
-			System.out.print("|");
 
-		}
+    // preset of obstacle
+    public void presetObstacles() {
 
-		//border bottom settings
-		System.out.println();
-		for(int bt = 0; bt<cols+2 ; bt++) {
-			System.out.print("_");
-		}
-	}
-	
-	
-	public void setObstacles() {
-		int[][] obs = {
-				{1,2},
-				{1,3},
-				{1,4},
-				{3,2},
-				{2,3},
-		};
-		
-		for(int i = 0; i < obs.length; i++) {
-			int r=obs[i][0];
-			int c=obs[i][1];
-			map[r][c].setObstacle(true);
-			
-				
-		}
-	}
-	
-	 public double checkExploredPercentage() {
+        int[][] preset = { { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, { 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 }, { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, };
+
+        for (int prow = 0; prow < rows; prow++) {
+            for (int pcol = 0; pcol < cols; pcol++) {
+                if (preset[prow][pcol] == 1)
+                    map[pcol][prow].setObstacle(true);
+            }
+        }
+
+    }
+
+    public void printMapObstacle() {
+        // border top settings
+        for (int bt = 0; bt < cols + 2; bt++) {
+            System.out.print("_");
+        }
+
+        for (int r = 0; r < rows; r++) {
+            System.out.println();
+            System.out.print("|");
+            for (int c = 0; c < cols; c++)
+                map[c][r].printGrid();
+            System.out.print("|");
+
+        }
+
+        // border bottom settings
+        System.out.println();
+        for (int bt = 0; bt < cols + 2; bt++) {
+            System.out.print("_");
+        }
+    }
+
+    public void setObstacles() {
+        int[][] obs = { { 1, 2 }, { 1, 3 }, { 1, 4 }, { 3, 2 }, { 2, 3 }, };
+
+        for (int i = 0; i < obs.length; i++) {
+            int r = obs[i][0];
+            int c = obs[i][1];
+            map[r][c].setObstacle(true);
+
+        }
+    }
+
+    public double checkExploredPercentage() {
         double totalCells = 0.0;
         double cellsExplored = 0.0;
         for (int x = 0; x < MAP_COLS; x++) {
@@ -165,8 +136,8 @@ public class GridMap {
         }
         return (cellsExplored / totalCells) * 100;
     }
-	 
-	 public void reset() {
+
+    public void reset() {
         for (int x = 0; x < MAP_COLS; x++) {
             for (int y = 0; y < MAP_ROWS; y++) {
                 if (!isInStartZone(x, y) && !isInEndZone(x, y))
@@ -178,8 +149,8 @@ public class GridMap {
     }
 
     public void loadFromDisk(String path) throws IOException {
-    	this.reset();
-
+        this.reset();
+        System.out.println(path);
         BufferedReader reader = new BufferedReader(new FileReader(path));
 
         for (int i = 0; i < MAP_ROWS; i++) {
@@ -195,7 +166,7 @@ public class GridMap {
         }
     }
 
-    //for real run
+    // for real run
     public void setObstacleProbability(int x, int y, int value) {
         if (isOutOfArena(x, y))
             return;
@@ -223,8 +194,8 @@ public class GridMap {
         for (int i = 0; i < part1.length() / 4; i++) {
             builder.append(Integer.toHexString(Integer.parseInt(part1.substring(i * 4, (i + 1) * 4), 2)));
         }
-        //System.out.println("Map descriptor part 1:");
-        //System.out.println(builder.toString());
+        // System.out.println("Map descriptor part 1:");
+        // System.out.println(builder.toString());
 
         return builder.toString();
     }
--- simInterface/LoadMapButtonListener.java
+++ simInterface/LoadMapButtonListener.java
@@ -1,6 +1,5 @@
 package simInterface;
 
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -11,51 +10,50 @@ import java.io.IOException;
 import entities.GridMap;
 import entities.Robot;
 
-
-public class LoadMapButtonListener implements ActionListener{
-
+public class LoadMapButtonListener implements ActionListener {
 
 	private Simulator curSim;
 	private GridMap curGrid;
 	private Robot curRobot;
-	
+
 	public LoadMapButtonListener(Simulator sim, Robot robot, GridMap grid) {
 		curSim = sim;
 		curRobot = robot;
 		curGrid = grid;
 		sim.addLoadMapButtonListener(this);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("LOADMAP PRESSED ");
 		new ConnectWorker().execute();
 	}
-	
-	class ConnectWorker extends SwingWorker<Integer,Integer>{
-		
+
+	class ConnectWorker extends SwingWorker<Integer, Integer> {
+
 		@Override
-	    protected Integer doInBackground() throws Exception {
-			String file_path = JOptionPane.showInputDialog(null,"Please input a map.","Map Loader", JOptionPane.QUESTION_MESSAGE);
-			if(null!=file_path) {
+		protected Integer doInBackground() throws Exception {
+			String file_path = JOptionPane.showInputDialog(null, "Please input a map.", "Map Loader",
+					JOptionPane.QUESTION_MESSAGE);
+			if (null != file_path) {
 				try {
-					curGrid.loadFromDisk("maps/" + file_path);
+					curGrid.loadFromDisk("D:/3004/mdpAlgo/maps/" + file_path);
 					System.out.println("Loaded map" + file_path);
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
-	                JOptionPane.showMessageDialog(null, "The file doesn't exist!", "Map loader", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(null, "The file doesn't exist!", "Map loader",
+							JOptionPane.ERROR_MESSAGE);
 					e1.printStackTrace();
 				}
-				
+
 			}
-	        return 1;
-	    }
-	
-	    @Override
-	    protected void done() {
-	        super.done();
-	    }
+			return 1;
+		}
+
+		@Override
+		protected void done() {
+			super.done();
+		}
 	}
-	
-	
+
 }
